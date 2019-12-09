4cba2c88ca73ad1de466405c0dd67dd1e2802291
rajat <rajat_kumar@outlook.com>
2019-12-09 20:28:13 +0530

    deployment

--- codesimilarity/settings.py
+++ codesimilarity/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'codesimilarity.urls'
